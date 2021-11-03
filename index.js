b2c35a40accacb69be1ba2bd7d673e5d0a82dc54
Brilliant-Developer <email2obaidul@gmail.com>
2021-11-04 01:00:50 +0600

    jwt added

--- index.js
+++ index.js
@@ -3,17 +3,41 @@ const { MongoClient } = require('mongodb'); // mongodb data
 require('dotenv').config(); // for secure env data
 const cors = require('cors');  // For cors bloking
 const ObjectId = require('mongodb').ObjectId;
+var admin = require("firebase-admin");
 
 const app = express();
+const port = process.env.PORT || 5000;
+
+// Firebase admin initialization
+
+var serviceAccount = require("./ema-john-474a1-firebase-adminsdk-rzvu0-3172e46b27.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 app.use(cors())      // For cors blocking
 app.use(express.json());
 
 
-const port = process.env.PORT || 5000;
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ezvo3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// To add token
+async function verifyToken(req, res, next){
+  if(req.headers?.authorization?.startsWith('Bearer ')){
+    const idToken = req.headers.authorization.split('Bearer ')[1]; //bearer ke token theke alada korar jonno
+    try{
+      const decodedUser = await admin.auth().verifyIdToken(idToken);
+      req.decodedUserEmail = decodedUser.email;
+    }
+    catch{
+
+    }
+  }
+  next();
+}
 // Insert a Document to mongo data base
 async function run() {
     try {
@@ -53,18 +77,24 @@ async function run() {
       });
 
       // Get Orders from server
-      app.get("/orders", async (req,res) => {
-        const cursor = orderCollection.find({});
-        const orders = await cursor.toArray();
-        res.json(orders) ;
+      app.get("/orders",verifyToken, async (req,res) => {
+        const email = req.query.email;
+        if(req.decodedUserEmail === email){
+          const query = {email: email}
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.json(orders) ;
+        } else{
+          res.status(401).json({Message: 'User Not authorized'})
+        }
+        
       })
       // Add Orders
       app.post('/orders', async(req, res) => {
+        
         const order = req.body;
         order.createdAt = new Date();
-        
         const result = await orderCollection.insertOne(order)
-        console.log(result);
         res.json(result);
       })
     } finally {
@@ -80,3 +110,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log("Server Running on", port);
 })
+/* 
+Get  Orders from server without id token er code
+const email = req.query.email;
+if(email){
+          query = {email: email}
+        }
+        const cursor = orderCollection.find(query);
+        const orders = await cursor.toArray();
+        res.json(orders) ;
+*/
